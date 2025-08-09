115a18a752a3f49ee72daa9a885eeb8d8dbd569e
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2025-08-09 17:50:48 +0300

    Update Products.jsx

--- src/pages/ads/Products.jsx
+++ src/pages/ads/Products.jsx
@@ -65,7 +65,7 @@ function Categories() {
   return (
     <>
       <Container
-        title={"Products"}
+{/*         title={"Products"} */}
         additionalHeaderContent={
           <div className="flex items-center gap-x-4">
             <Search
@@ -89,6 +89,7 @@ function Categories() {
 }
 
 export default Categories;
+
 
 
 
